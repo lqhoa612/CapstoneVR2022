6c52722817c9ac219697baa80aa7643037216170
lqhoa612 <lqhoa612@gmail.com>
2022-08-12 18:56:05 +1000

    Update to MxLabSimVR2022
    - add decoration
    - add gripper control

--- MxLabSim2022/Assets/Scripts/UR_Scripts/URController.cs
+++ MxLabSim2022/Assets/Scripts/UR_Scripts/URController.cs
@@ -10,7 +10,7 @@ public class URController : MonoBehaviour
     [HideInInspector] public int m_index;
 
     private float m_timerA, m_timerB;
-    private readonly int[] m_revolutionJoints = {2,3,4,5,6,7,10,11,13,15,16,18};
+    private readonly int[] m_revolutionJoints = {2,3,4,5,6,7,10};
 
     // Update is called once per frame
     void Update()
@@ -48,12 +48,12 @@ public class URController : MonoBehaviour
             5 => "Wrist 1",
             6 => "Wrist 2",
             7 => "Wrist 3",
-            10 => "Right Inner Knuckle",
-            11 => "Right Outer Knuckle",
-            13 => "Right Inner Finger",
-            15 => "Left Inner Knuckle",
-            16 => "Left Outer Knuckle",
-            18 => "Left Inner Finger",
+            10 => "Gripper",
+            //11 => "Right Outer Knuckle",
+            //13 => "Right Inner Finger",
+            //15 => "Left Inner Knuckle",
+            //16 => "Left Outer Knuckle",
+            //18 => "Left Inner Finger",
             _ => "Shoulder",
         };
     }
