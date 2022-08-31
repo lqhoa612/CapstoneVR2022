f8dbcecd26c932bb0256e3d1381272c975e7c6cf
Hoa Le <lqhoa612@gmail.com>
2022-08-31 16:14:25 +1000

    update from ros

--- ROS/ur3_unity_ros/src/Ur3Service.py
+++ ROS/ur3_unity_ros/src/Ur3Service.py
@@ -4,21 +4,24 @@ import spatialmath as sm
 import math as m
 import numpy as np
 import rospy
-
 from ur3_unity_ros.srv import *
 
 def plan(req):
+    #Initial robot model
     ur3 = rtb.models.UR3()
+    #Initial joint angles coordinate
     ur3.q = [m.radians(-90), m.radians(-90), m.radians(-90), m.radians(-90), m.radians(90), m.radians(0)]
+    #Target pose
     Tep = sm.SE3.Trans(-0.25, 0.25, 0.35) * sm.SE3.Eul([0, m.radians(90), m.radians(0)])
+    #Get IK solution
     sol = ur3.ikine_LMS(Tep, ur3.q)
+    #Convert rad to deg and round to int
     rs = [  np.round(m.degrees(sol[0][0])), np.round(m.degrees(sol[0][1])), 
             np.round(m.degrees(sol[0][2])), np.round(m.degrees(sol[0][3])),     
             np.round(m.degrees(sol[0][4])), np.round(m.degrees(sol[0][5]))]
-    
     # ok = rtb.jtraj(ur3.q, sol[0], 4)
     print(rs)
-
+    #Send response to unity
     return TrajPlannerResponse(rs)
 
 def Ur3Service():
@@ -34,7 +37,6 @@ def Ur3Service():
     #         m.degrees(sol[0][2]), m.degrees(sol[0][3]),     
     #         m.degrees(sol[0][4]), m.degrees(sol[0][5])]
     # print(rs)
-
 
     rospy.spin()
 
