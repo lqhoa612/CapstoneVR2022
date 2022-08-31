b03de4e277f747406ade72bc886e8ce8dcb1a850
Hoa Le <lqhoa612@gmail.com>
2022-08-31 14:29:08 +1000

    commit from ros

--- ROS/ur3_unity_ros/src/Ur3Service.py
+++ ROS/ur3_unity_ros/src/Ur3Service.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
-from ast import For
 import roboticstoolbox as rtb
 import spatialmath as sm
-import spatialgeometry as sg
 import math as m
 import numpy as np
 import rospy
@@ -10,34 +8,16 @@ import rospy
 from ur3_unity_ros.srv import *
 
 def plan(req):
-    #Init the model
     ur3 = rtb.models.UR3()
-    #Match the unity pose and save as previous pose
-    prev_q = req.unity
-    # prev_q = [  m.radians(prev_q[0]), m.radians(prev_q[1]),
-    #             m.radians(prev_q[2]), m.radians(prev_q[3]),
-    #             m.radians(prev_q[4]), m.radians(prev_q[5])  ]
-    # prev_q[0] += m.radians(-90)
-    # prev_q[1] += m.radians(-90)
-    # prev_q[2] += m.radians(-90)
-    # prev_q[3] += m.radians(-90)
-    # prev_q[4] += m.radians(90)
-
     ur3.q = [m.radians(-90), m.radians(-90), m.radians(-90), m.radians(-90), m.radians(90), m.radians(0)]
-    Tep = sm.SE3.Trans(-0.25, 0.25, 0.2) * sm.SE3.Eul([0, m.radians(90), m.radians(0)])
+    Tep = sm.SE3.Trans(-0.25, 0.25, 0.35) * sm.SE3.Eul([0, m.radians(90), m.radians(0)])
     sol = ur3.ikine_LMS(Tep, ur3.q)
-    rs = [  m.degrees(sol[0][0]), m.degrees(sol[0][1]), 
-            m.degrees(sol[0][2]), m.degrees(sol[0][3]),     
-            m.degrees(sol[0][4]), m.degrees(sol[0][5])]
+    rs = [  np.round(m.degrees(sol[0][0])), np.round(m.degrees(sol[0][1])), 
+            np.round(m.degrees(sol[0][2])), np.round(m.degrees(sol[0][3])),     
+            np.round(m.degrees(sol[0][4])), np.round(m.degrees(sol[0][5]))]
     
     # ok = rtb.jtraj(ur3.q, sol[0], 4)
     print(rs)
-    #msg = TrajPlannerResponse(rs)
-    #msg.ros = rs
-    # res = []
-    # res.insert(msg)
-    # for i in range(len(res)):
-    #     res[i].data = ok.q[i]
 
     return TrajPlannerResponse(rs)
 
@@ -48,7 +28,7 @@ def Ur3Service():
 
     # ur3 = rtb.models.UR3()
     # ur3.q = [m.radians(-90), m.radians(-90), m.radians(-90), m.radians(-90), m.radians(90), m.radians(0)]
-    # Tep = sm.SE3.Trans(-0.25, 0.25, 0.2) * sm.SE3.Eul([0, m.radians(90), m.radians(0)])
+    # Tep = sm.SE3.Trans(-0.25, 0.25, 0.35) * sm.SE3.Eul([0, m.radians(90), m.radians(0)])
     # sol = ur3.ikine_LMS(Tep, ur3.q)
     # rs = [  m.degrees(sol[0][0]), m.degrees(sol[0][1]), 
     #         m.degrees(sol[0][2]), m.degrees(sol[0][3]),     
